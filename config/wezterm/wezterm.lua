fe52b2d4b463145009ee54d857ca6cf592597fd6
eliascasher <ehcastroh@berkeley.edu>
2026-07-20 15:41:03 -0700

    wezterm font-size 11.0

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,8 +3,8 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "rose-pine-moon"
 config.font = wezterm.font("Hack Nerd Font")
-config.font_size = 15.0
-config.window_background_opacity = 0.80
+config.font_size = 11.0
+config.window_background_opacity = 0.90
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
